458310045c02f89740f20a3437456c57e8efa8cf
Hilmar Berger <berger@mpiib-berlin.mpg.de>
2019-11-29 14:15:19 +0100

    added DNA metadata

---
+++ Data/Metadata/DNA/Mouse2/extract_per_sample_files.py
@@ -0,0 +1,35 @@
+import sys, os
+
+ifile = sys.stdin
+
+per_sample_files = {}
+for line in ifile:
+    fasta_file = line.strip("\n\r")
+    run = os.path.split(os.path.dirname(fasta_file))[-1]
+    ff = os.path.basename(fasta_file).replace("_f.fastq.gz","").replace(".fastq.gz","")
+    ff_fields = ff.split("_")
+    sample = "_".join(ff_fields[0:2])
+    run = ff_fields[2]
+    index = ff_fields[3]
+    lane = ff_fields[5]
+    pair_end = ff_fields[6]
+    part = ff_fields[7]
+
+    fkey = (sample)
+    complete_key = (sample,run,lane,part)
+
+    if not fkey in per_sample_files:
+        per_sample_files[fkey] = {"R1": {}, "R2": {}}
+
+    per_sample_files[fkey][pair_end][complete_key] = fasta_file
+
+for s in sorted(per_sample_files.keys()):
+    reps = sorted(per_sample_files[s]["R1"].keys())
+    r1 = []
+    r2 = []
+    for r in reps:
+        r1.append(per_sample_files[s]["R1"][r])
+        r2.append(per_sample_files[s]["R2"][r])
+    print "%s\t%s\t%s" % (s, ",".join(r1), ",".join(r2) )
+    #ofile.write("%s\t%s\t%s\n" % ("_".join(s), ",".join(r1), ",".join(r2) ) )
+
